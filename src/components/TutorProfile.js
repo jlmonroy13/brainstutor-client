2c1a4d747113405256a6866c5caf284d8bcc0845
jlmonroy13 <jlmonroy13@gmail.com>
2017-03-06 21:25:22 -0500

    adding model-react package and creating modal previous send message to tutor

--- src/components/TutorProfile.js
+++ src/components/TutorProfile.js
@@ -3,14 +3,22 @@ import Footer from './Footer';
 import { Link } from 'react-router';
 import Gravatar from 'react-gravatar';
 import { showUser } from '../requests/users';
+import ReactModal from 'react-modal';
+import TextFieldGroup from './TextFieldGroup';
 
 class TutorProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tutor: {},
+      showModal: false,
+      isSignUp: true,
     };
 
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.activateSignUp = this.activateSignUp.bind(this);
+    this.activateLogIn = this.activateLogIn.bind(this);
   }
 
   componentWillMount() {
@@ -20,9 +28,26 @@ class TutorProfile extends React.Component {
     });
   }
 
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+    
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  activateLogIn() {
+    this.setState({ isSignUp: false });
+  }
+    
+  activateSignUp() {
+    this.setState({ isSignUp: true });
+  }
+
   render() {
-    const { tutor } = this.state;
+    const { tutor, isSignUp } = this.state;
     const { profile } = tutor;
+
     return (
       <div>
         <div className="profile__container">
@@ -53,7 +78,10 @@ class TutorProfile extends React.Component {
                   <div className="box-message__body">
                     <label className="main-form__label main-form__label--large">Tu mensaje*</label>
                     <textarea className="main-form__textarea push--bottom" />
-                    <button className="button button--dark-green button--block button--large">{`Contactar a ${tutor.first_name}`}</button>
+                    <button
+                      className="button button--dark-green button--block button--large"
+                      onClick={this.handleOpenModal}
+                    >{`Contactar a ${tutor.first_name}`}</button>
                   </div>
                 </div>
                 <div className="grid">
@@ -68,6 +96,89 @@ class TutorProfile extends React.Component {
             </div>
           </div>
         </div>
+        <ReactModal 
+          isOpen={this.state.showModal}
+          className="Modal"
+          overlayClassName="Overlay"
+        >
+          <div className="Modal__content">
+            <div className="Modal__header">
+              <h2 className="Modal__header-title">Tu mensaje esta listo para ser enviado</h2>
+              <span className="Modal__btn-close" onClick={this.handleCloseModal}>&#120;</span>
+            </div>
+            <div className="Modal__body">
+              <form onSubmit={this.onSubmitForm} autoComplete="off">
+                {isSignUp ?
+                  <div>
+                    <p className="Modal__body-description">Para poder enviar tu mensaje y recibir respuesta, por favor crea una cuenta gratuita.</p>
+                    <TextFieldGroup
+                      value={this.state.firstName}
+                      onChange={this.onChangeForm}
+                      type="text"
+                      field="firstName"
+                      label="Nombre"
+                    />
+                    <TextFieldGroup
+                      value={this.state.lastName}
+                      onChange={this.onChangeForm}
+                      type="text"
+                      field="lastName"
+                      label="Apellido"
+                    />
+                    <TextFieldGroup
+                      value={this.state.email}
+                      onChange={this.onChangeForm}
+                      type="email"
+                      field="email"
+                      label="Correo Electrónico"
+                    />
+                    <TextFieldGroup
+                      value={this.state.password}
+                      onChange={this.onChangeForm}
+                      type="password"
+                      field="password"
+                      label="Contraseña"
+                    />
+                    <TextFieldGroup
+                      value={this.state.passwordConfirmation}
+                      onChange={this.onChangeForm}
+                      type="password"
+                      field="passwordConfirmation"
+                      label="Confirmar Contraseña"
+                    />
+                    <button
+                      className={`button button--large button--block button--blue push-half--top`}
+                    >Registrarse</button>
+                    <p className="Modal__body-footer">¿Ya tienes una cuenta? <span className="Modal__body-footer-link" onClick={this.activateLogIn}>¡Ingresa ya!</span></p>
+                  </div>
+                :
+                  <div>
+                    <p className="Modal__body-description">Para poder enviar tu mensaje y recibir respuesta, por favor ingresa con tu cuenta.</p>
+                    <TextFieldGroup
+                      value={this.state.email}
+                      onChange={this.onChangeForm}
+                      type="email"
+                      field="email"
+                      label="Correo Electrónico"
+                    />
+                    <TextFieldGroup
+                      value={this.state.password}
+                      onChange={this.onChangeForm}
+                      type="password"
+                      field="password"
+                      label="Contraseña"
+                    />
+                    <button
+                      className={`button button--large button--block button--blue push-half--top`}
+                    >Ingresar</button>
+                    <p className="Modal__body-footer">¿Aún no tienes una cuenta? <span className="Modal__body-footer-link" onClick={this.activateSignUp}>¡Registrate!</span></p>
+                  </div>
+                }
+                
+              </form>
+            </div>
+          </div>
+        </ReactModal>
         <Footer />
       </div>
     );
