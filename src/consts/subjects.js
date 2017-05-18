78323dadb89a19a1acbe30ae7d7a0cb785115bc4
Juan David Girlado Rodriguez <jdgiraldo88@hotmail.com>
2017-05-18 12:32:56 -0500

    Sort subjects list

--- src/consts/subjects.js
+++ src/consts/subjects.js
@@ -1,4 +1,6 @@
-const subjectsLocal = ['Física Mecánica', 'Cálculo Integral', 'Cálculo Vectorial', 'Cálculo Diferencial', 'Álgebra Lineal', 'Física Moderna', 'Circuitos Eléctricos', 'Química Básica', 'Física Eléctrica', 'Física Calor Ondas', 'Estadística', 'Ecuaciones Diferenciales', 'Estática', 'Algebra y Trigonometría', 'Estructura de Datos', 'Geometría Analítica', 'Dinámica', 'Termodinámica', 'Fluidos', 'Algoritmo y Programación', 'Materiales para Ingeniería', 'Matemáticas Especiales'];
+let subjectsLocal = ['Física Mecánica', 'Cálculo Integral', 'Cálculo Vectorial', 'Cálculo Diferencial', 'Álgebra Lineal', 'Física Moderna', 'Circuitos Eléctricos', 'Química Básica', 'Física Eléctrica', 'Física Calor Ondas', 'Estadística', 'Ecuaciones Diferenciales', 'Estática', 'Algebra y Trigonometría', 'Estructura de Datos', 'Geometría Analítica', 'Dinámica', 'Termodinámica', 'Fluidos', 'Algoritmo y Programación', 'Materiales para Ingeniería', 'Matemáticas Especiales'];
+
+subjectsLocal = subjectsLocal.sort();
 
 export default subjectsLocal;
 
